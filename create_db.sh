0efad62cbec73583ee8d85eb43fc2aa08c0add77
Kai Chang <kai.s.chang@gmail.com>
2015-08-19 18:47:01 -0700

    sql directory

--- create_db.sh
+++ create_db.sh
@@ -12,16 +12,16 @@ sed -i.bak "s/µ/mc/" data/NUTR_DEF.txt
 rm data/*.bak
 
 echo "Loading food group..."
-sqlite3 $1 < load_food_group.sql
+sqlite3 $1 < sql/load_food_group.sql
 
 echo "Loading food..."
-sqlite3 $1 < load_food.sql
+sqlite3 $1 < sql/load_food.sql
 
 echo "Loading nutrient..."
-sqlite3 $1 < load_nutrient.sql
-sqlite3 $1 < load_common_nutrient.sql
+sqlite3 $1 < sql/load_nutrient.sql
+sqlite3 $1 < sql/load_common_nutrient.sql
 
 echo "Loading nutrition..."
-sqlite3 $1 < load_nutrition.sql
+sqlite3 $1 < sql/load_nutrition.sql
 
 echo "Done."
